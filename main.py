fbab6f497b77d97e9ebb50a43031eb3d6bc94a4c
Jack Finnis <jack.finnis@icloud.com>
2023-12-09 03:18:16 +0000

    Added Flask app

--- main.py
+++ main.py
@@ -4,31 +4,34 @@ import firebase_admin
 from firebase_admin import messaging
 from firebase_admin.credentials import Certificate
 from firebase_admin.messaging import Message, Notification, APNSConfig, APNSPayload, Aps
+from flask import Flask
+app = Flask(__name__)
 
-credential = Certificate("credential.json")
-app = firebase_admin.initialize_app(credential)
+@app.route('/')
+def run():
+    firebase_admin.initialize_app(Certificate("credential.json"))
 
-telegraph = "https://www.telegraph.co.uk"
-response = requests.get(telegraph)
-html = response.text
-soup = BeautifulSoup(html, "html.parser")
-cartoon = soup.select("img[alt='Matt cartoon']")[0]
-url = cartoon["src"].split("?")[0]
+    response = requests.get("https://www.telegraph.co.uk")
+    soup = BeautifulSoup(response.text, "html.parser")
+    cartoon = soup.select("img[alt='Matt cartoon']")[0]
+    url = cartoon["src"].split("?")[0]
 
-with open("url.txt", "r") as file:
-    old_url = file.read()
-with open("url.txt", "w") as file:
-    file.write(url)
+    with open("url.txt", "r") as file:
+        old_url = file.read()
+    with open("url.txt", "w") as file:
+        file.write(url)
 
-if url != old_url:
-    with open("urls.txt", "a") as file:
-        file.write(url + '\n')
-    
-    topic = "new_cartoon"
-    title = "New Cartoon!"
-    body = "View today's Matt Cartoon"
-    headers = { "apns-collapse-id": topic }
-    apns = APNSConfig(payload=APNSPayload(aps=Aps(badge=1)), headers=headers)
-    notification = Notification(title=title, body=body)
-    message = Message(notification=notification, topic=topic, apns=apns)
-    messaging.send(message)
+    if url != old_url:
+        with open("urls.txt", "a") as file:
+            file.write(url + '\n')
+        
+        topic = "new_cartoon"
+        title = "New Cartoon!"
+        body = "View today's Matt Cartoon"
+        headers = { "apns-collapse-id": topic }
+        apns = APNSConfig(payload=APNSPayload(aps=Aps(badge=1)), headers=headers)
+        notification = Notification(title=title, body=body)
+        message = Message(notification=notification, topic=topic, apns=apns)
+        messaging.send(message)
+
+app.run('0.0.0.0')
